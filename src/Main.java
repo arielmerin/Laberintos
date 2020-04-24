fca2e214d17844b3241edddc6a04410bac04dec6
ArmandoAAC11 <armandoaac@ciencias.unam.mx>
2020-04-24 18:02:37 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import static com.UIMenu.UIMenu.principal;
 /**
  * <h1> Clase Main </h1>
  * Clase principal desde la cual se efectuan las llamadas a clases secundarias en la ejecuacion del programa.
- * @author Ariel Merino, Armando Aquino
+ * @author Ariel Merino, Armando Aquino Chapa
  * @version 1.0
  */
 public class Main {
